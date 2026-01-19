44f9191171b31fc8af0d2d71601beb8759dd9140
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-01-19 14:31:54 +0530

    Final Completion

--- Guess my no/script.js
+++ Guess my no/script.js
@@ -1,6 +1,6 @@
 let luckyNum = Number(Math.ceil(Math.random() * 20));
 let score = 5;
-let highscore = 0;
+let highScore = 0;
 // console.log(luckyNum);
 let tops = document.querySelector(".top");
 
@@ -10,10 +10,19 @@ show.classList.add("show-no");
 tops.appendChild(show);
 show.style.display = "none";
 
+let changeResult = function (result) {
+    document.querySelector(".result").textContent = result;
+};
+
+let changeHead = function (heading) {
+    document.querySelector("#heading").textContent = heading;
+};
+
 let play = document.querySelector(".st-btn");
 play.addEventListener("click", function () {
     // console.log("clicked");
-    document.querySelector("#heading").textContent = "Guessing...";
+    // document.querySelector("#heading").textContent = "Guessing...";
+    changeHead("Guessing...");
     play.remove();
     show.style.display = "block";
     inputNum();
@@ -29,56 +38,52 @@ const inputNum = () => {
             document.querySelector(".input").value = "";
             // console.log(value, typeof value);
             if (!value) {
-                document.querySelector(".result").textContent = "No Number...";
+                // document.querySelector(".result").textContent = "No Number...";
+                changeResult("No Number...");
             } else if (value == luckyNum) {
-                document.querySelector(".result").textContent = "YOU WIN";
+                // document.querySelector(".result").textContent = "YOU WIN";
+                changeResult("YOU WIN");
                 document.querySelector("body").style.animation =
                     "color-change-2x 3s linear infinite alternate both";
-                document.querySelector("#heading").textContent =
-                    "CONGRATULATIONS!";
+                // document.querySelector("#heading").textContent =
+                // "CONGRATULATIONS!";
+                changeHead("CONGRATULATIONS!");
+
                 document.querySelector(".target").textContent =
                     "Increase your Score...";
                 show.textContent = luckyNum;
 
-                if (highscore < score) {
-                    highscore = score;
+                if (highScore < score) {
+                    highScore = score;
                     document.querySelector(".hscore-no").textContent = score;
                 }
-            } else if (value <= luckyNum) {
-                if (score > 1) {
-                    document.querySelector(".result").textContent =
-                        "Too Low...";
-                    score--;
-                    document.querySelector(".score-no").textContent = score;
-                } else {
-                    document.querySelector(".result").textContent = "YOU LOST";
-                    document.querySelector("body").style.background = "#222";
-                    document.querySelector("body").style.color = "#fff";
-                    document.querySelector("#heading").textContent =
-                        "GAME OVER!";
-                    document.querySelector(".target").textContent =
-                        "Well Played...";
-                    document.querySelector(".rst-btn").style.color = "#fff";
-                    show.textContent = luckyNum;
-                    document.querySelector(".score-no").textContent = 0;
-                }
-            } else if (value >= luckyNum) {
-                if (score > 1) {
-                    document.querySelector(".result").textContent =
-                        "Too High...";
-                    score--;
-                    document.querySelector(".score-no").textContent = score;
-                } else {
-                    document.querySelector(".result").textContent = "YOU LOST";
-                    document.querySelector("body").style.background = "#222";
-                    document.querySelector("body").style.color = "#fff";
-                    document.querySelector("#heading").textContent =
-                        "GAME OVER!";
-                    document.querySelector(".target").textContent =
-                        "Well Played...";
-                    document.querySelector(".rst-btn").style.color = "#fff";
-                    show.textContent = luckyNum;
-                    document.querySelector(".score-no").textContent = 0;
+            } else {
+                if (value != luckyNum) {
+                    if (score > 1) {
+                        // document.querySelector(".result").textContent =
+                        //     value < luckyNum ? "Too Low..." : "Too High...";
+                        changeResult(
+                            value < luckyNum ? "Too Low..." : "Too High...",
+                        );
+                        score--;
+                        document.querySelector(".score-no").textContent = score;
+                    } else {
+                        // document.querySelector(".result").textContent = "YOU LOST";
+                        changeResult("YOU LOST");
+                        document.querySelector("body").style.background =
+                            "#222";
+                        document.querySelector("body").style.animation = "none";
+                        document.querySelector("body").style.color = "#fff";
+                        // document.querySelector("#heading").textContent =
+                        // "GAME OVER!";
+                        changeHead("GAME OVER!");
+
+                        document.querySelector(".target").textContent =
+                            "Well Played...";
+                        document.querySelector(".rst-btn").style.color = "#fff";
+                        show.textContent = luckyNum;
+                        document.querySelector(".score-no").textContent = 0;
+                    }
                 }
             }
             // show.textContent = value;
@@ -92,8 +97,11 @@ let reset = document
         show.textContent = "?";
         score = 5;
         luckyNum = Number(Math.ceil(Math.random() * 20));
-        document.querySelector("#heading").textContent = "Guessing...";
-        document.querySelector(".result").textContent = "Your Results...";
+        // document.querySelector("#heading").textContent = "Guessing...";
+        // document.querySelector(".result").textContent = "Your Results...";
+        changeHead("Guessing...");
+
+        changeResult("Your Results...");
         document.querySelector(".target").textContent = "Guess (1 to 20)";
         document.querySelector(".score-no").textContent = 5;
         document.querySelector("body").style.background = "#2a7b9b";
