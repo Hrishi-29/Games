3ce33cb6712148fdd0dc6f9648b83c59f76710b9
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-01-30 16:19:44 +0530

    adding functionality of final finishing the game

--- PIG Game/app.js
+++ PIG Game/app.js
@@ -3,28 +3,28 @@
 let start = document.querySelector(".st-btn");
 let restart = document.querySelector(".rs-btn");
 let die = document.querySelector(".dice");
-let lBox = document.querySelector(".box-1");
-let rBox = document.querySelector(".box-2");
+let box0 = document.querySelector(".box-0");
+let box1 = document.querySelector(".box-1");
 let lScore = document.querySelector(".score0");
 let rScore = document.querySelector(".score1");
 
 const leftAnime = () => {
-    lBox.style.animation =
+    box0.style.animation =
         "swing-in-right-bck 0.6s cubic-bezier(0.175, 0.885, 0.320, 1.275) both";
     setTimeout(() => {
-        lBox.style.animation = "none";
+        box0.style.animation = "none";
     }, 700);
 };
 
 const rightAnime = () => {
-    rBox.style.animation =
+    box1.style.animation =
         "swing-in-left-bck 0.6s cubic-bezier(0.175, 0.885, 0.320, 1.275) both";
     setTimeout(() => {
-        rBox.style.animation = "none";
+        box1.style.animation = "none";
     }, 700);
 };
 start.addEventListener("click", () => {
-    console.log("clicked to Start...");
+    console.log("Game Start...");
     start.classList.toggle("hidden");
     restart.classList.toggle("hidden");
     die.classList.toggle("hidden");
@@ -33,69 +33,98 @@ start.addEventListener("click", () => {
     setTimeout(() => {
         die.style.animation = "none";
     }, 250);
-    lBox.classList.toggle("slider");
+    box0.classList.toggle("slider");
     leftAnime();
-    rBox.classList.toggle("slider");
+    box1.classList.toggle("slider");
     shuffling();
 });
 
 let shuffle = document.querySelector(".sff-btn");
 let hold = document.querySelector(".h-btn");
 const shuffling = () => {
+    const scores = [0, 0];
     let tScore = 0;
     let active = 0;
+    let playing = true;
     shuffle.addEventListener("click", () => {
-        console.log("shuffling...");
-        let rnum = Number(Math.ceil(Math.random() * 6));
-        die.style.backgroundImage = `url('dice/${rnum}.png')`;
-        if (rnum != 1) {
-            tScore += rnum;
-            document.querySelector(`.score${active}`).textContent = tScore;
-            console.log(tScore);
-        } else {
-            tScore = 0;
-            document.querySelector(`.score${active}`).textContent = tScore;
-            active = active === 0 ? 1 : 0;
-            if (active != 0) {
-                rBox.classList.toggle("slider");
-                rightAnime();
-                lBox.classList.toggle("slider");
+        if (playing) {
+            console.log("shuffling...");
+            let rnum = Number(Math.ceil(Math.random() * 6));
+            die.style.backgroundImage = `url('dice/${rnum}.png')`;
+            if (rnum != 1) {
+                tScore += rnum;
+                document.querySelector(`.score${active}`).textContent = tScore;
+                // console.log(tScore);
             } else {
-                lBox.classList.toggle("slider");
-                leftAnime();
-                rBox.classList.toggle("slider");
+                tScore = 0;
+                document.querySelector(`.score${active}`).textContent = tScore;
+                active = active === 0 ? 1 : 0;
+                if (active != 0) {
+                    box1.classList.toggle("slider");
+                    rightAnime();
+                    box0.classList.toggle("slider");
+                } else {
+                    box0.classList.toggle("slider");
+                    leftAnime();
+                    box1.classList.toggle("slider");
+                }
             }
+            die.style.animation =
+                "rotate-in-center 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) infinite both";
+            setTimeout(() => {
+                die.style.animation = "none";
+            }, 600);
         }
-        die.style.animation =
-            "rotate-in-center 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) infinite both";
-        setTimeout(() => {
-            die.style.animation = "none";
-        }, 600);
     });
     hold.addEventListener("click", () => {
-        console.log("Holding...");
-        if (active != 0) {
-            console.log(document.querySelector(`.score${active}`).textContent);
-            document.querySelector(`.s--${active}`).textContent =
-                document.querySelector(`.score${active}`).textContent;
-        } else {
-            console.log(document.querySelector(`.score${active}`).textContent);
-            document.querySelector(`.s--${active}`).textContent =
-                document.querySelector(`.score${active}`).textContent;
-        }
-        rnum = 1;
-        die.style.backgroundImage = `url('dice/${rnum}.png')`;
-        tScore = 0;
-        document.querySelector(`.score${active}`).textContent = tScore;
-        active = active === 0 ? 1 : 0;
-        if (active != 0) {
-            rBox.classList.toggle("slider");
-            rightAnime();
-            lBox.classList.toggle("slider");
-        } else {
-            lBox.classList.toggle("slider");
-            leftAnime();
-            rBox.classList.toggle("slider");
+        if (playing) {
+            console.log("Holding...");
+            if (active != 0) {
+                scores[active] += tScore;
+                // console.log(tScore);
+                document.querySelector(`.s--${active}`).textContent =
+                    scores[active];
+            } else {
+                scores[active] += tScore;
+                // console.log(tScore);
+                document.querySelector(`.s--${active}`).textContent =
+                    scores[active];
+            }
+            if (scores[active] != 50 && scores[active] <= 49) {
+                rnum = 1;
+                die.style.backgroundImage = `url('dice/${rnum}.png')`;
+                tScore = 0;
+                document.querySelector(`.score${active}`).textContent = tScore;
+                active = active === 0 ? 1 : 0;
+                if (active != 0) {
+                    box1.classList.toggle("slider");
+                    rightAnime();
+                    box0.classList.toggle("slider");
+                } else {
+                    box0.classList.toggle("slider");
+                    leftAnime();
+                    box1.classList.toggle("slider");
+                }
+            } else {
+                console.log(`Winner...${active}`);
+                tScore = 0;
+                document.querySelector(`.score${active}`).textContent = tScore;
+                document
+                    .querySelector(`.box-${active}`)
+                    .classList.toggle("slider");
+                document
+                    .querySelector(`.box-${active}`)
+                    .classList.add("winner");
+                document.querySelector(".winner").style.animation =
+                    "swing-in-top-bck 0.6s cubic-bezier(0.175, 0.885, 0.320, 1.275) both";
+                document
+                    .querySelector(`.box-${(active = active === 0 ? 1 : 0)}`)
+                    .classList.add("loser");
+                document.querySelector(".loser").style.animation =
+                    "swing-in-top-bck 0.6s cubic-bezier(0.175, 0.885, 0.320, 1.275) both";
+
+                playing = false;
+            }
         }
     });
 };
